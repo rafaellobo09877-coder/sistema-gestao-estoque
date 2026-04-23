dd546d3d1c06b7fb9764e05f498e0b6425b52bcb
RAFAEL ARAUJO ALMEIDA LOBO <rafael.lobo@semps.pms.ad>
2026-04-23 16:16:42 -0300

    frontend corrigido

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -8,10 +8,7 @@ api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
   if (token) {
-    config.headers = {
-      ...config.headers,
-      Authorization: `Bearer ${token}`
-    };
+    config.headers.Authorization = `Bearer ${token}`;
   }
 
   return config;
